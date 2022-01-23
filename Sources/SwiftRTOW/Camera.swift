74f51f2fb7765c60f842c2f614c49b155d9e3db4
otabuzzman <iuergen.schuck@gmail.com>
2022-01-23 18:33:16 +0100

    Save work on camera definitions

--- Sources/SwiftRTOW/Camera.swift
+++ Sources/SwiftRTOW/Camera.swift
@@ -1,2 +1,33 @@
 class Camera {
+	private var eye = P(x: 0, y: 0, z: 0)
+	private var aperture: Float = 0
+
+	var u = V(x: 0, y: 0, z: 0)
+	var v = V(x: 0, y: 0, z: 0)
+	var w = V(x: 0, y: 0, z: 0)
+	var hvec = V(x: 0, y: 0, z: 0)
+	var wvec = V(x: 0, y: 0, z: 0)
+	var dvec = V(x: 0, y: 0, z: 0)
+
+	func set(eye: P, pat: P, vup: V, fov: Float, aspratio: Float, aperture: Float, fostance: Float) {
+		self.eye = eye
+		self.aperture = aperture
+
+		w = unitV(v: eye-pat )
+		u = unitV(v: cross(a: vup, b: w))
+		v = cross(a: w, b: u)
+
+		let h = 2.0*tan(0.5*fov*kPi/180.0)
+		let W = h*aspratio
+		hvec = fostance*h/2.0*v
+		wvec = fostance*W/2.0*u
+		dvec = fostance*w
+	}
+
+	func ray(s: Float, t: Float) -> Ray {
+		let r = aperture/2.0*rndVin1disk()
+		let o = r.x*u+r.y*v
+
+		return Ray(ori: eye+o, dir: s*wvec+t*hvec-dvec-o )
+	}
 }
--- Sources/SwiftRTOW/Unknown.swift
+++ Sources/SwiftRTOW/Unknown.swift
@@ -1,1 +1,5 @@
 // not defined in Swift on Winos
+
+func tan(_ rad: Float) -> Float {
+	return 0.176
+}
