f755cf4172c8883ea0d71a6c38f6624c6b5d4fc5
Jürgen Schuck <otabuzzman@users.noreply.github.com>
2022-01-30 11:18:23 +0100

    Fix pixel type change

--- Sources/SwiftRTOW/UIImage.swift
+++ Sources/SwiftRTOW/UIImage.swift
@@ -1,18 +1,18 @@
 import SwiftUI
 
 extension UIImage {
-	convenience init?(imageData: [Rtow.RGBA8], imageWidth: Int, imageHeight: Int) {
+	convenience init?(imageData: [Pixel], imageWidth: Int, imageHeight: Int) {
 		guard imageWidth>0 && imageHeight>0, imageData.count == imageWidth*imageHeight else {
 			return nil
 		}
 
 		guard let dataProvider = (imageData.withUnsafeBytes { (dataPointee: UnsafeRawBufferPointer) -> CGDataProvider? in
-			return CGDataProvider(data: Data(bytes: dataPointee.baseAddress!, count: imageData.count*MemoryLayout<Rtow.RGBA8>.size) as CFData)
+			return CGDataProvider(data: Data(bytes: dataPointee.baseAddress!, count: imageData.count*MemoryLayout<Pixel>.size) as CFData)
 		}) else {
 			return nil
 		}
 
-		guard let image = CGImage( width: imageWidth, height: imageHeight, bitsPerComponent: 8, bitsPerPixel: 32, bytesPerRow: imageWidth*MemoryLayout<Rtow.RGBA8>.size, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue), provider: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent) else {
+		guard let image = CGImage( width: imageWidth, height: imageHeight, bitsPerComponent: 8, bitsPerPixel: 32, bytesPerRow: imageWidth*MemoryLayout<Pixel>.size, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue), provider: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent) else {
 			return nil
 		}
 
