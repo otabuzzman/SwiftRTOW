8bf366c8cc54852acf768de283a20bd7360d7b91
otabuzzman <iuergen.schuck@gmail.com>
2022-02-08 13:10:13 +0100

    Fix scenes from chapters 8 and 10

--- Sources/SwiftRTOW/Ch10.swift
+++ Sources/SwiftRTOW/Ch10.swift
@@ -2,13 +2,13 @@ class Ch10: @unchecked Sendable, Things {
     var things: [Thing]! = []
     
     func load() {
-        things.append(Sphere(center: P(x: 0, y: -100.5, z: -1), radius: 100, optics: Diffuse(albedo: C(x: 0.8, y: 0.8, z: 0))))
+        things.append(Sphere(center: P(x: 0, y: -100.0, z: -1.0), radius: 100.0, optics: Diffuse(albedo: C(x: 0.8, y: 0.8, z: 0))))
         
-        things.append(Sphere(center: P(x: 0, y: 0, z: -1), radius: 0.5, optics: Diffuse(albedo: C(x: 0.1, y: 0.2, z: 0.5))))
+        things.append(Sphere(center: P(x: 0, y: 1.0, z: 0), radius: 1.0, optics: Diffuse(albedo: C(x: 0.1, y: 0.2, z: 0.5))))
         
-        things.append(Sphere(center: P(x: -1, y: 0, z: -1), radius: 0.5, optics: Refract(index: 1.5)))
-        things.append(Sphere(center: P(x: -1, y: 0, z: -1), radius: -0.4, optics: Refract(index: 1.5)))
+        things.append(Sphere(center: P(x: -4, y: 1.0, z: 0), radius: 1.0, optics: Refract(index: 1.5)))
+        things.append(Sphere(center: P(x: -4, y: 1.0, z: 0), radius: -0.8, optics: Refract(index: 1.5)))
         
-        things.append(Sphere(center: P(x: 1, y: 0, z: -1), radius: 0.5, optics: Reflect(albedo: C(x: 0.8, y: 0.6, z: 0.2), fuzz: 0)))
+        things.append(Sphere(center: P(x: 4.0, y: 1.0, z: 0), radius: 1.0, optics: Reflect(albedo: C(x: 0.8, y: 0.6, z: 0.2), fuzz: 0)))
     }
 }
--- Sources/SwiftRTOW/Ch8.swift
+++ Sources/SwiftRTOW/Ch8.swift
@@ -2,8 +2,8 @@ class Ch8: @unchecked Sendable, Things {
     var things: [Thing]! = []
     
     func load() {
-        things.append(Sphere(center: P(x: 0, y: -100.5, z: -1), radius: 100, optics: Diffuse(albedo: C(x: 0.5, y: 0.5, z: 0.5))))
+        things.append(Sphere(center: P(x: 0, y: -100.0, z: -1.0), radius: 100.0, optics: Diffuse(albedo: C(x: 0.5, y: 0.5, z: 0.5))))
         
-        things.append(Sphere(center: P(x: 0, y: 0, z: -1), radius: 0.5, optics: Diffuse(albedo: C(x: 0.5, y: 0.5, z: 0.5))))
+        things.append(Sphere(center: P(x: 0, y: 1.0, z: 0), radius: 1.0, optics: Diffuse(albedo: C(x: 0.5, y: 0.5, z: 0.5))))
     }
 }
