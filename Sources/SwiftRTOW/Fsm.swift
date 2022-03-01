9b0e398987122bcc517cc950e4d42b4330068129
otabuzzman <iuergen.schuck@gmail.com>
2022-03-01 12:05:34 +0100

    Save work on UI FSM 4th draft

--- Sources/SwiftRTOW/Fsm.swift
+++ Sources/SwiftRTOW/Fsm.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 enum FsmState: Int {
-    case CAM, LOD, MOV, OPT, ROL, VSC, VWR, ZOM
+    case CAM, LOD, MOV, OPT, TRN, VSC, VWR, ZOM
 }
-let FsmStateName = ["CAM", "LOD", "MOV", "OPT", "ROL", "VSC", "VWR", "ZOM"]
+let FsmStateName = ["CAM", "LOD", "MOV", "OPT", "TRN", "VSC", "VWR", "ZOM"]
 
 enum FsmEvent: Int {
-    case CAM, CTL, LOD, MOV, OPT, RET, ROL, VWR, ZOM
+    case CAM, CTL, LOD, MOV, OPT, RET, TRN, VWR, ZOM
 }
-let FsmEventName = ["CAM", "CTL", "LOD", "MOV", "OPT", "RET", "ROL", "VWR", "ZOM"]
+let FsmEventName = ["CAM", "CTL", "LOD", "MOV", "OPT", "RET", "TRN", "VWR", "ZOM"]
 
 typealias FsmAction = () throws -> Void
 
@@ -17,25 +17,34 @@ class Fsm: ObservableObject {
     @Published private(set) var hEvent = FsmHEvent()
     
     @Published private(set) var movAmount = CGSize.zero
+    private var camMovAmount = CGSize.zero
+    private var camMovRecall = CGSize.zero
+    private var startJumpMov: CGSize? = nil
     
     private var eaTable: [[FsmAction]] = [[]]
     private var eaParam = EaParam()
     
     var isCam: Bool { get { hState.peek() as? FsmState == FsmState.CAM } }
     var isLod: Bool { get { hState.peek() as? FsmState == FsmState.LOD } }
+    var isMov: Bool { get { hState.peek() as? FsmState == FsmState.MOV } }
     var isOpt: Bool { get { hState.peek() as? FsmState == FsmState.OPT } }
+    var isTrn: Bool { get { hState.peek() as? FsmState == FsmState.TRN } }
     var isVwr: Bool { get { hState.peek() as? FsmState == FsmState.VWR } }
     var isVsc: Bool { get { hState.peek() as? FsmState == FsmState.VSC } }
+    var isZom: Bool { get { hState.peek() as? FsmState == FsmState.ZOM } }
+    
+    var isCsl: Bool { get { isVwr || isCam || isOpt } }
+    var isCad: Bool { get { isMov || isTrn || isZom } }
     
     init(startWithState state: FsmState = FsmState.VSC) {
         self.hState.push(state)
         self.eaTable = [
-            /* S/E     CAM       CTL       LOD       MOV       OPT       RET       ROL       VWR       ZOM     */
+            /* S/E     CAM       CTL       LOD       MOV       OPT       RET       TRN       VWR       ZOM     */
             /* CAM */ [eaReject, eaReject, eaReject, eaCamMov, eaReject, eaCamRet, eaReject, eaReject, eaReject],
             /* LOD */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaLodRet, eaReject, eaReject, eaReject],
-            /* MOV */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject],
+            /* MOV */ [eaReject, eaReject, eaReject, eaMovMov, eaReject, eaMovRet, eaReject, eaReject, eaReject],
             /* OPT */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaOptRet, eaReject, eaReject, eaReject],
-            /* ROL */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject],
+            /* TRN */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject],
             /* VSC */ [eaReject, eaVscCtl, eaVscLod, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject],
             /* VWR */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaVwrRet, eaReject, eaReject, eaReject],
             /* ZOM */ [eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject, eaReject]
@@ -55,14 +64,43 @@ class Fsm: ObservableObject {
         try eaTable[s][e]()
     }
     
-    private var dirMovAmount = CGSize.zero
-    private var dirMovRecall = CGSize.zero
-    private func eaCamMov() {
-        let movAmount = eaParam.pop() as! CGSize
-        dirMovAmount = dirMovRecall+movAmount
-        self.movAmount = dirMovAmount
+    private func eaMovRet() {
+        timeoutTask = runOnTimeout(seconds: 3) {
+            do {
+                try self.transition(event: FsmEvent.RET)
+            } catch {}
+        }
         
-        update(withState: FsmState.CAM)
+        camMovRecall = camMovAmount
+        startJumpMov = nil
+        
+        hState.pop()
+        hEvent.pop()
+        
+        update(withState: hState.peek() as! FsmState)
+    }
+    
+    private func eaMovMov() {
+        let movAmount = eaParam.pop() as! CGSize
+        camMovAmount = startJumpMov!+camMovRecall+movAmount
+        self.movAmount = camMovAmount
+        
+        update(withState: FsmState.MOV)
+    }
+    
+    private func eaCamMov() {
+        timeoutTask.cancel()
+        
+        let movAmount = eaParam.pop() as! CGSize
+        
+        if startJumpMov == nil {
+            startJumpMov = -movAmount
+        } else {
+            camMovAmount = startJumpMov!+camMovRecall+movAmount
+            self.movAmount = camMovAmount
+        }
+        
+        update(withState: FsmState.MOV, noHistory: false)
     }
     
     private func eaVwrRet() {
@@ -77,14 +115,20 @@ class Fsm: ObservableObject {
         update(withState: FsmState.VSC)
     }
     
+    let timeoutTask: Task<Void, Never>!
     private func eaVscCtl() {
-        let pressedSideButton = eaParam.pop() as! ButtonType
-        
-        runOnTimeout(seconds: 3) {
+        timeoutTask = runOnTimeout(seconds: 3) {
             do {
                 try self.transition(event: FsmEvent.RET)
             } catch {}
         }
+        
+        movAmount = CGSize.zero
+        camMovAmount = CGSize.zero
+        camMovRecall = CGSize.zero
+        startJumpMov = nil
+        
+        let pressedSideButton = eaParam.pop() as! ButtonType
         
         let nextState: FsmState
         switch pressedSideButton {
@@ -101,11 +145,12 @@ class Fsm: ObservableObject {
         update(withState: nextState)
     }
     
+    let outbackTask: Task<Void, Never>!
     private func eaVscLod() {
         let raycer = eaParam.pop() as! Rtow
         let things = eaParam.pop() as! Things
         
-        runInBackground {
+        outbackTask = runInOutback {
             let numRowsAtOnce = ProcessInfo.processInfo.processorCount/2*3
             await raycer.render(numRowsAtOnce: numRowsAtOnce, things: things)
             do {
@@ -140,16 +185,16 @@ class Fsm: ObservableObject {
         print("new state \(FsmStateName[state.rawValue]) (S/E history \(hState.count)/\(hEvent.count))")
     }
     
-    private func runOnTimeout(seconds: Int, closure: @escaping () -> Void) {
-        if seconds>0 {
-            Task {
+    private func runOnTimeout(seconds: Int, closure: @escaping () -> Void) -> Task<Void, Never> {
+        Task {
+            do {
                 try await Task.sleep(nanoseconds: UInt64(seconds*1_000_000_000))
                 closure()
-            }
+            } catch {}
         }
     }
     
-    private func runInBackground(closure: @escaping () async -> Void) {
+    private func runInOutback(closure: @escaping () async -> Void) -> Task<Void, Never> {
         Task {
             await closure()
         }
